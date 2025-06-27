6cd1d326c36f4b1df6af7a733a16ccd92538ee88
Ram <Ram@gmail.com>
2025-06-27 22:42:13 +0530

    commiting project 8 and 9

---
+++ wikipedia_project9/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,HttpResponse
+
+import wikipedia
+
+def home(request):
+    if request.method=='POST':
+        search=request.POST['search']
+        try:
+            result=wikipedia.summary(search,sentences=3)
+
+        except:
+            return HttpResponse("wrong input")    
+        return render(request,'main/index.html',{'result':result})
+    return render(request,'main/index.html')
