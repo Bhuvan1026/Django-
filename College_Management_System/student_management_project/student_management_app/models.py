0af5df24fce5df08ffc19880b29cecc5bf7d9e56
Ram <Ram@gmail.com>
2025-07-09 21:08:11 +0530

    All models declared,views declared ,StudentViews started with student_home

--- College_Management_System/student_management_project/student_management_app/models.py
+++ College_Management_System/student_management_project/student_management_app/models.py
@@ -67,4 +67,105 @@ class Students(models.Model):
     updated_at=models.DateTimeField(auto_now=True)
     objects=models.Manager()
 
+class Attendance(models.Model):
+    id=models.AutoField(primary_key=True)
+    subject_id=models.ForeignKey(Subjects,on_delete=models.DO_NOTHING)
+    attenance_date=models.DateField()
+    session_year_id=models.ForeignKey(SessionYearModel,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
 
+class AttendanceReport(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    attendance_id=models.ForeignKey(Attendance,on_delete=models.CASCADE)
+    status=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+class LeaveReportStudent(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    leave_date=models.CharField(max_length=233)
+    leave_message=models.TextField()
+    leave_status=models.IntegerField(default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+class LeaveReportStaff(models.Model):
+    id=models.AutoField(primary_key=True)
+    staff_id=models.ForeignKey(Staffs,on_delete=models.CASCADE)
+    leave_date=models.CharField(max_length=255)
+    leave_message=models.TextField()
+    leave_status=models.IntegerField(default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+class FeedBackStudent(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    feedback=models.TextField()
+    feedback_reply=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+class FeedBackStaffs(models.Model):
+    id=models.AutoField(primary_key=True)
+    staff_id=models.ForeignKey(Staffs,on_delete=models.CASCADE)
+    feedback=models.TextField()
+    feedback_reply=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+class NotificationStudent(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    message=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+class NotificationStaffs(models.Model):
+    id=models.AutoField(primary_key=True)
+    staff_id=models.ForeignKey(Staffs,on_delete=models.CASCADE)
+    message=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+class StudentResult(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    subject_id=models.ForeignKey(Subjects,on_delete=models.CASCADE,default=1)
+    subject_exam_marks=models.FloatField(default=0)
+    subject_assignment_marks=models.FloatField(default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+@receiver(post_save,sender=CustomUser)
+
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        if instance.user_type==1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type==2:
+            Staffs.objects.create(admin=instance)
+        if instance.user_type==3:
+            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_year_id=SessionYearModel.objects.get(id=1),address='',profile_pic="",gender='')
+
+@receiver(post_save,sender=CustomUser)
+
+def save_user_profile(sender,instance,**kwargs):
+    if instance.user_type==1:
+        instance.adminhod.save()
+    if instance.user_type==2:
+        instance.staffs.save()
+    if instance.user_type==3:
+        instance.students.save()
