dcb74b750dbbd35d4ff47272174fc841ae6a409c
Ram <Ram@gmail.com>
2025-07-11 21:36:39 +0530

    Added view(student_view_attendance

--- College_Management_System/student_management_project/student_management_app/StudentViews.py
+++ College_Management_System/student_management_project/student_management_app/StudentViews.py
@@ -37,3 +37,12 @@ def student_home(request):
 
         }
         return render(request,'student_template/student_home_template.html')
+    
+def student_view_attendance(request):
+    student=Students.objects.get(admin=request.user.id)
+    course=student.course_id
+    subjects=Subjects.objects.filter(course_id=course)
+    context={
+        "subjects":subjects
+    }    
+    return render(request,'student_template/student_view_attendance.html',context)
